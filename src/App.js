9f84854b8f945f2c4118e217788e36e1e1d1ef1e
AndreaGirondo <andreagirondo@gmail.com>
2023-05-09 17:16:24 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [villes, setVilles] = useState([]);
+
+  const handleInput = (event) => {
+    const cp = event.target.value;
+    if (cp.length === 5) {
+      const url = `https://geo.api.gouv.fr/communes?codePostal=${cp}&fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre`;
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          const villesData = data.map((ville) => {
+            return {
+              nom: ville.nom,
+              codePostal: ville.codesPostaux[0],
+              codeDepartement: ville.codeDepartement,
+              codeRegion: ville.codeRegion,
+              population: ville.population,
+            };
+          });
+          setVilles(villesData);
+        });
+    }
+  };
+
   return (
-    <div className="App">
+    <div className="App">   
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>API de code postal</h1>
+        <form> 
+            <label htmlFor="cp">Code postal</label>
+            <div className="Input">
+            <input 
+              type="number"
+              maxLength="5"
+              id="cp"
+              className="form-control"
+              name="cp"
+              onInput={handleInput}
+            />
+            </div>
+        </form>
+        <ul>
+          {villes.map((ville) => (
+            <li key={ville.codePostal}>
+              <strong>{ville.nom} </strong> ({ville.codePostal}) - {ville.population} habitants
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
 }
+
+//AG.
 
 export default App;
